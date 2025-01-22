900b7a6966c443502053a5ba05996fcb7ac6b154
Luis Jambrina <ljam   git config --global user.name Laura>
2025-01-22 18:37:12 +0100

    Updates

---
+++ precache-manifest.166f79414adfd5a2510d6c13356f9b15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2b60546cc0825a63a0d39181fca49cc",
+    "url": "/WhosWho/index.html"
+  },
+  {
+    "revision": "65d3770dda5cefd75a06",
+    "url": "/WhosWho/static/css/main.ffcc60c9.chunk.css"
+  },
+  {
+    "revision": "8ab0cddc758f8a1a1f22",
+    "url": "/WhosWho/static/js/2.e38fbaa2.chunk.js"
+  },
+  {
+    "revision": "b3f8f07ddacecead5d18a7b46a3cf869",
+    "url": "/WhosWho/static/js/2.e38fbaa2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65d3770dda5cefd75a06",
+    "url": "/WhosWho/static/js/main.296da25e.chunk.js"
+  },
+  {
+    "revision": "a988fa123b4b98be30ff",
+    "url": "/WhosWho/static/js/runtime-main.d531757a.js"
+  }
+]);
